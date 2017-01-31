d5f0eefc3feb94f068beed49df92e4d5e023bc2a
S <s>
2017-01-31 13:14:12 -0800

    {"types":"C","runs":3}

--- Breakout.java
+++ Breakout.java
@@ -89,14 +89,15 @@ private void placeBricks() {
 	 * One for loop should make the program draw two rows of the same color of bricks.
 	 */
 	
-	for (int i=0;i<NBRICKS_PER_ROW;i++){
-		for (int k = i; k < NBRICK_ROWS; k ++){
-			brick.setFilled(true);
-			brick.setColor(Color.RED);
-			double leftX = (getWidth() - BRICK_WIDTH*NBRICKS_PER_ROW - BRICK_SEP*(NBRICKS_PER_ROW-1))*0.5;
-			add (brick, leftX + i* (BRICK_WIDTH + BRICK_SEP),BRICK_Y_OFFSET + k * (BRICK_HEIGHT + BRICK_SEP));
-			
-		}
+
+	
+	for (int i = 0; i < NBRICKS_PER_ROW; i++){
+		double leftX = (getWidth() - BRICK_WIDTH * NBRICKS_PER_ROW - BRICK_SEP * (NBRICKS_PER_ROW-1))*0.5;
+		int y = BRICK_Y_OFFSET;
+		
+		brick.setFilled(true);
+		brick.setColor(Color.RED);
+		add (brick, leftX, y);
 	}
 	
 	
