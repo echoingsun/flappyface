f23284965fd3ae11c1afb14cb2d7c26d32d49826
S <s>
2017-01-31 13:16:22 -0800

    {"types":"C","runs":5}

--- Breakout.java
+++ Breakout.java
@@ -91,13 +91,13 @@ private void placeBricks() {
 	
 
 	
-	for (int i = 0; i < NBRICKS_PER_ROW; i++){
+	for (int i = 1; i < NBRICKS_PER_ROW; i++){
 		double leftX = (getWidth() - BRICK_WIDTH * NBRICKS_PER_ROW - BRICK_SEP * (NBRICKS_PER_ROW-1))*0.5;
 		int y = BRICK_Y_OFFSET;
 		
 		brick.setFilled(true);
 		brick.setColor(Color.RED);
-		add (brick, leftX + (i-1) * (BRICK_WIDTH + BRICK_SEP), y);
+		add (brick, leftX + (i * (BRICK_WIDTH + BRICK_SEP), y);
 	}
 	
 	
