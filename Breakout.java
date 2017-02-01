a971fa46c4fde74e77345731120a88073e4472b4
S <s>
2017-01-31 17:44:43 -0800

    {"types":"C","runs":82}

--- Breakout.java
+++ Breakout.java
@@ -148,7 +148,7 @@ private void play() {
 
 private boolean hitPaddle() {
 	GObject obj = getElementAt(ball.getX() + BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);	
-	return ball.getY() + 2*BALL_RADIUS >= paddle.getY() && obj == paddle;
+	return ball.getY() + 2*BALL_RADIUS >= paddle.getY() && (obj == paddle && obj != ball);
 }
 
 private boolean hitCeiling() {
