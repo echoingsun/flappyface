f80e27d70b7b3e9beacdf670f9354cbb1ff782bd
S <s>
2017-02-03 10:32:12 -0800

    {"types":"C","runs":124}

--- Extension_Breakout.java
+++ Extension_Breakout.java
@@ -50,10 +50,10 @@ public class Extension_Breakout extends GraphicsProgram {
 	private static final int PADDLE_Y_OFFSET = 30;
 
 	/** Number of bricks per row */
-	private static final int NBRICKS_PER_ROW = 10;
+	private static final int NBRICKS_PER_ROW = 1;
 
 	/** Number of rows of bricks */
-	private static final int NBRICK_ROWS = 10;
+	private static final int NBRICK_ROWS = 2;
 
 	/** Separation between bricks */
 	private static final int BRICK_SEP = 4;
