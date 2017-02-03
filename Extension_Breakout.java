cf0fe8cb5d74534ba0ca30a8c77504b8cfd3fb53
S <s>
2017-02-02 19:35:14 -0800

    {"types":"C","runs":98}

--- Extension_Breakout.java
+++ Extension_Breakout.java
@@ -406,6 +406,7 @@ public class Extension_Breakout extends GraphicsProgram {
 	}
 	
 	private void updatePoints() {
+		pts = (NBRICKS_PER_ROW*NBRICK_ROWS - count)*POINT_PER_BRICK;
 		points.setLocation(getWidth() - points.getWidth()-5, 15);
 		points.setLabel(pts + " pts");
 	}
