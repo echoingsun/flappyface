e47018b698e54bcf64f9b9fb569a6da937578b62
S <s>
2017-03-07 13:03:54 -0800

    {"types":"C","runs":54}

--- Contest_Run.java
+++ Contest_Run.java
@@ -31,8 +31,8 @@ public class Contest_Run extends Program implements Constants {
 		boolean bottomLeft = sky.getElementAt(aBird.getImg().getX() -1, aBird.getImg().getY() + aBird.getImg().getHeight() +1) == null;
 		
 		GObject obj = sky.getElementAt(aBird.getImg().getX() + aBird.getImg().getWidth() + 1, aBird.getImg().getY() -1);
-		boolean uR = obj == null;
-		boolean upperRight = sky.getElementAt(aBird.getImg().getX() + aBird.getImg().getWidth() + 1, aBird.getImg().getY() -1) != sky.getImg();
+		boolean uR = obj == sky.getIm();
+		boolean upperRight = sky.getElementAt(aBird.getImg().getX() + aBird.getImg().getWidth() + 1, aBird.getImg().getY() -1) == null;
 		boolean bottomRight = sky.getElementAt(aBird.getImg().getX() + aBird.getImg().getWidth() + 1, aBird.getImg().getY() + aBird.getImg().getHeight() +1) == null;
 		boolean top = aBird.getImg().getY() -1 <= 0;
 		boolean bottom = aBird.getImg().getY() + aBird.getImg().getHeight() + 1 >= sky.getHeight();
