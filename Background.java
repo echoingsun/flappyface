f673114a1696fa6157c7d11e23a95347d4ddf487
S <s>
2017-03-07 11:03:04 -0800

    {"types":"C","runs":20}

--- Background.java
+++ Background.java
@@ -4,14 +4,11 @@ import acm.program.*;
 
 public class Background extends GCanvas implements Constants{
 
-
-	
 	public void init (){
 		addBackground();
 	}
 
-	private void addBackground() {
-		
+	private void addBackground() {		
 		GImage img = new GImage ("background.jpg");
 		img.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 		add(img);
