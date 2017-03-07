1cc0afc49fcbc2770bf864dc3f33aec78bfb3a51
S <s>
2017-03-07 11:26:23 -0800

    {"types":"C","runs":28}

--- Constants.java
+++ Constants.java
@@ -2,4 +2,7 @@
 public interface Constants {
 	public static final int APPLICATION_WIDTH = 1200;
 	public static final int APPLICATION_HEIGHT = 712;
+	
+	public static final int BIRD_WIDTH = 50;
+	public static final int BIRD_HEIGHT = 50;
 }
