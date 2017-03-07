19bd6770db2000e968b48b3d10043a20abd521f9
S <s>
2017-03-07 10:45:34 -0800

    {"types":"C","runs":12}

--- Contest_Run.java
+++ Contest_Run.java
@@ -9,13 +9,15 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Contest_Run extends GraphicsProgram implements Constants{
-
+	
+	private Background sky;
+	
 	public void init (){
 		setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 	}
 	
 	public void run (){
-		Background sky = new Background ("background.jpg");
+		sky = new Background ("background.jpg");
 	}
 
 }
