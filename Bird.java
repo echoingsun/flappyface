2787ee18327bbbf1d3b266e1264dc3c10c06a8ee
S <s>
2017-03-07 11:22:58 -0800

    {"types":"C","runs":25}

--- Bird.java
+++ Bird.java
@@ -1,11 +1,17 @@
 import acm.graphics.*;
 
-
-public class Bird implements Constants{
+public class Bird implements Constants {
 
 	private GImage img;
-	
-	public Bird () {
-		
+
+	public Bird() {
+		img = new GImage("karel.png");
 	}
+
+	public GImage getImg() {
+		return img;
+	}
+
+	
+	
 }
