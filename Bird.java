33108fb7c1c46b353f80096de20480c5b0ee344f
S <s>
2017-03-07 12:43:17 -0800

    {"types":"C","runs":49}

--- Bird.java
+++ Bird.java
@@ -26,20 +26,7 @@ public class Bird implements Constants {
 		this.img.move(vx, vy);
 	}
 
-	public boolean notHit(Background sky){
-		/*GObject obj = sky.getElementAt(img.getX()-1, img.getY() -1);*/
-		boolean upperLeft = sky.getElementAt(img.getX()-1, img.getY() -1) == null;
-		boolean bottomLeft = sky.getElementAt(img.getX() -1, img.getY() + img.getHeight() +1) == null;
-		boolean upperRight = sky.getElementAt(img.getX() + img.getWidth() + 1, img.getY() -1) == null;
-		boolean bottomRight = sky.getElementAt(img.getX() + img.getWidth() + 1, img.getY() + img.getHeight() +1) == null;
-		boolean top = img.getY() -1 <= 0;
-		boolean bottom = img.getY() + img.getHeight() + 1 >= sky.getHeight();
-		
-		if (upperLeft && bottomLeft && upperRight && bottomRight && !top && !bottom){
-			return true;
-		} 
-		return false;
-	}
+
 	
 	
 	
