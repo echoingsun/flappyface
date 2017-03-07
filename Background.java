3a927014f2c96b88240aae8844981190f2e4209c
S <s>
2017-03-07 10:43:03 -0800

    {"types":"C","runs":12}

--- Background.java
+++ Background.java
@@ -5,8 +5,8 @@ public class Background extends GCanvas implements Constants{
 	GImage img;
 
 	public Background (String fileName){
-		img = new GImage (fileName);
-		img.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+		this.img = new GImage (fileName);
+		this.img.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 		this.add(img,0,0);
 	}
 }
