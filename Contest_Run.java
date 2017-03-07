b9d17bb1788fa72a3b840536e7ece31dae379938
S <s>
2017-03-07 10:33:51 -0800

    {"types":"C","runs":10}

--- Contest_Run.java
+++ Contest_Run.java
@@ -10,9 +10,12 @@ import java.awt.event.*;
 
 public class Contest_Run extends GraphicsProgram implements Constants{
 
+	public void init (){
+		setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+	}
+	
 	public void run (){
-		println(getWidth());
-		println(getHeight());
+		println(getWidth()*1.0, getHeight()*1.0);
 	}
 
 }
