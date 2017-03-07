2376d0fb9a8a6a0dfed4aa2393d3745158b943be
S <s>
2017-03-07 10:59:53 -0800

    {"types":"C","runs":18}

--- Contest_Run.java
+++ Contest_Run.java
@@ -5,12 +5,13 @@ public class Contest_Run extends GraphicsProgram implements Constants{
 	
 	private Background sky = new Background();
 	
-/*	public void init (){
+	public void init (){
 		setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
-	}*/
+	}
 	
 	public void run (){
 		add(sky);
+		sky.init();
 		
 
 	}
