b33cc0cade39474c8d5d76203cbc86b2d2a17cf7
S <s>
2017-03-08 00:40:22 -0800

    {"types":"C","runs":38}

--- Contest_Run.java
+++ Contest_Run.java
@@ -23,7 +23,8 @@ public class Contest_Run extends Program implements Constants {
 		loadBird();
 		enableMouse();
 		blockPairs = sky.addBlockPairs(BLOCK_START_POINT);
-
+		sky.pairArray.add(blockPairs);
+		
 		
 		double distance = 0;
 		while (bird.notHit(sky)) {
