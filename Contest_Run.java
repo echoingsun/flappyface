eb19953818ae781b95fa677257486d26ae88fafa
S <s>
2017-03-08 01:05:24 -0800

    {"types":"C","runs":44}

--- Contest_Run.java
+++ Contest_Run.java
@@ -44,13 +44,13 @@ public class Contest_Run extends Program implements Constants {
 			}			
 			pause(DELAY);
 
-			
-/*			for (int i = 0; i < sky.pairArray.size(); i++){
-				if (sky.pairArray.get(i).getX() + BLOCK_WIDTH < 200){
-					blockPairs.remove(sky.pairArray.get(i));
-					//sky.remove(sky.pairArray.get(i));
+			if (sky.pairArray.get(0).getX() + BLOCK_WIDTH < 200){
+				blockPairs = null;
+				for (int i = 1; i < sky.pairArray.size(); i ++){
+					blockPairs.add(sky.pairArray.get(i));
 				}
-			}*/
+				sky.remove(sky.pairArray.get(0));
+			}
 			
 			
 		}
