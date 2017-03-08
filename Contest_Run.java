eab1c8e9b24954e2e69ff02a528a88891c678bb3
S <s>
2017-03-07 23:45:38 -0800

    {"types":"C","runs":23}

--- Contest_Run.java
+++ Contest_Run.java
@@ -33,8 +33,8 @@ public class Contest_Run extends Program implements Constants {
 			if (distance <= BLOCK_INTERVAL){
 				distance += Math.abs(blockPairs.vx);
 			} else {
-
-				double x = blockPairs.getX();
+				distance = 0;
+				
 				Pairs newBlockPairs = sky.addBlockPairs(blockPairs.getX() + BLOCK_INTERVAL);
 
 				blockPairs.add(newBlockPairs);
