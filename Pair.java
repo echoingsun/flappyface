b949c429fee448fbb1445eb096877f3d3cc8ded1
S <s>
2017-03-07 18:34:00 -0800

    {"types":"C","runs":22}

--- Pair.java
+++ Pair.java
@@ -4,8 +4,15 @@ import acm.util.RandomGenerator;
 public class Pair implements Constants{
 	
 	GCompound pair;
+	RandomGenerator rg = new RandomGenerator();
 	
-	public Pair (Blocks bottom, Blocks upper){
+	public Pair (){
+		Blocks bottom = new Blocks (false);
+		double bottomY = rg.nextDouble(APPLICATION_HEIGHT * 0.6, APPLICATION_HEIGHT * 0.8);
+		
+		Blocks upper = new Blocks (true);
+		double upperY = bottomY - rg.nextDouble (APPLICATION_HEIGHT * 0.3, APPLICATION_HEIGHT * 0.5) - upper.getImg().getHeight();
+		
 		pair.add(bottom.getImg());
 		pair.add(upper.getImg());
 		
@@ -13,24 +20,9 @@ public class Pair implements Constants{
 	
 	public void moveLeft(){
 		
-		RandomGenerator rg = new RandomGenerator();
+		
 		double vx = rg.nextDouble(-MAX_SPEED, -MIN_SPEED);
 		pair.move(vx, 0);
 	}
-	
-	/*
-	 * Method addBlocks add a pair of blocks to the screen.
-	 */
-	public void addBlocks() {
-		// add the bottom block.
-		Blocks bottom = new Blocks (false);
-		double bottomY = rg.nextDouble(this.getHeight() * 0.6, this.getHeight() * 0.8);
-		this.add(bottom.getImg(), BLOCK_START_POINT,bottomY);
-		
-		// add the upper block
-		Blocks upper = new Blocks (true);
-		double upperY = bottomY - rg.nextDouble (this.getHeight() * 0.3, this.getHeight() * 0.5) - upper.getImg().getHeight();
-		this.add(upper.getImg(), BLOCK_START_POINT, upperY);
 
-	}
 }
