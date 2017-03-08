860b1737718e869313b40040394b4c571a50476d
S <s>
2017-03-07 18:31:02 -0800

    {"types":"C","runs":22}

--- Pair.java
+++ Pair.java
@@ -1,4 +1,36 @@
+import acm.graphics.GCompound;
+import acm.util.RandomGenerator;
 
-public class Pair {
+public class Pair implements Constants{
+	
+	GCompound pair;
+	
+	public Pair (Blocks bottom, Blocks upper){
+		pair.add(bottom.getImg());
+		pair.add(upper.getImg());
+		
+	}
+	
+	public void moveLeft(){
+		
+		RandomGenerator rg = new RandomGenerator();
+		double vx = rg.nextDouble(-MAX_SPEED, -MIN_SPEED);
+		pair.move(vx, 0);
+	}
+	
+	/*
+	 * Method addBlocks add a pair of blocks to the screen.
+	 */
+	public void addBlocks() {
+		// add the bottom block.
+		Blocks bottom = new Blocks (false);
+		double bottomY = rg.nextDouble(this.getHeight() * 0.6, this.getHeight() * 0.8);
+		this.add(bottom.getImg(), BLOCK_START_POINT,bottomY);
+		
+		// add the upper block
+		Blocks upper = new Blocks (true);
+		double upperY = bottomY - rg.nextDouble (this.getHeight() * 0.3, this.getHeight() * 0.5) - upper.getImg().getHeight();
+		this.add(upper.getImg(), BLOCK_START_POINT, upperY);
 
+	}
 }
