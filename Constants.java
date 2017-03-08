8e2d62b0a5acf731e65ef88eb6632ae29fb323d9
S <s>
2017-03-07 17:47:18 -0800

    {"types":"C","runs":9}

--- Constants.java
+++ Constants.java
@@ -6,6 +6,9 @@ public interface Constants {
 	public static final int BLOCK_HEIGHT = 793;
 	public static final int BLOCK_WIDTH = 138;
 	
+	public static final int BLOCK_START_POINT = 300;
+	public static final int BLOCK_INTERVAL = 100;
+	
 	public static final int BIRD_WIDTH = 50;
 	public static final int BIRD_HEIGHT = 50;
 	
