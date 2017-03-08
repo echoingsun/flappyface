bf2d48c280950bf8675e0686f5ad5befee1c5351
S <s>
2017-03-08 00:52:22 -0800

    {"types":"C","runs":43}

--- Contest_Run.java
+++ Contest_Run.java
@@ -44,9 +44,6 @@ public class Contest_Run extends Program implements Constants {
 			}			
 			pause(DELAY);
 
-			if (sky.pairArray.get(0).getX() + BLOCK_WIDTH < 200){
-				sky.remove(sky.pairArray.get(0));
-			}
 			
 /*			for (int i = 0; i < sky.pairArray.size(); i++){
 				if (sky.pairArray.get(i).getX() + BLOCK_WIDTH < 200){
