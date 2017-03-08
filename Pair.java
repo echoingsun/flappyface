09f180b9996fcc868166dac1818dd6574570dc5a
S <s>
2017-03-07 19:41:48 -0800

    {"types":"C","runs":40}

--- Pair.java
+++ Pair.java
@@ -18,7 +18,9 @@ public class Pair extends GCompound implements Constants{
 		double upperY = bottomY - rg.nextDouble(0.3*APPLICATION_HEIGHT, 0.6*APPLICATION_HEIGHT) - upper.getImg().getHeight();
 				
 		sky.add(upper.getImg(),BLOCK_START_POINT, upperY);
-		pair.add(bottom.getImg(), BLOCK_START_POINT, bottomY);
+		sky.add(bottom.getImg(), BLOCK_START_POINT, bottomY);
 
+		pair.add(upper.getImg());
+		pair.add(bottom.getImg());
 	}
 }
