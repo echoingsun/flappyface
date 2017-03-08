d17014d6d7e32ee5c81cc0dc51c08d08f5b9f46d
S <s>
2017-03-07 22:19:20 -0800

    {"types":"C","runs":0}

--- Pair.java
+++ Pair.java
@@ -7,15 +7,19 @@ public class Pair extends GCompound implements Constants{
 	double vx = rg.nextDouble(-MAX_SPEED, -MIN_SPEED);
 
 	public Pair (){
-				
-		Blocks bottom = new Blocks (false);
-		Blocks upper = new Blocks (true);
 		
-		double bottomY = rg.nextDouble (0.6*APPLICATION_HEIGHT, 0.8*APPLICATION_HEIGHT);
-		double upperY = bottomY - rg.nextDouble(0.3*APPLICATION_HEIGHT, 0.6*APPLICATION_HEIGHT) - upper.getImg().getHeight();
+		for (int i =0; i < PAIR_NUM; i ++){
+			Blocks bottom = new Blocks (false);
+			Blocks upper = new Blocks (true);
+			
+			double bottomY = rg.nextDouble (0.6*APPLICATION_HEIGHT, 0.8*APPLICATION_HEIGHT);
+			double upperY = bottomY - rg.nextDouble(0.3*APPLICATION_HEIGHT, 0.6*APPLICATION_HEIGHT) - upper.getImg().getHeight();
+					
+			this.add(upper.getImg(),BLOCK_START_POINT +  BLOCK_INTERVAL * i, upperY);
+			this.add(bottom.getImg(), BLOCK_START_POINT +  BLOCK_INTERVAL * i, bottomY);
+		}
 				
-		this.add(upper.getImg(),BLOCK_START_POINT, upperY);
-		this.add(bottom.getImg(), BLOCK_START_POINT, bottomY);
+		
 	}
 	
 	public void moveLeft(){
