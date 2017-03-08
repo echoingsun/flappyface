f73674b0fa17f37e34341ba80cfdf59bc4d68515
S <s>
2017-03-07 19:51:44 -0800

    {"types":"C","runs":46}

--- Pair.java
+++ Pair.java
@@ -4,15 +4,13 @@ import acm.util.RandomGenerator;
 public class Pair extends GCompound implements Constants{
 	GCompound pair;
 	RandomGenerator rg = new RandomGenerator();
-	
-	Blocks upper;
-	Blocks bottom;
+
 	
 	public Pair (){
 		pair = new GCompound();
 		
-		bottom = new Blocks (false);
-		upper = new Blocks (true);
+		Blocks bottom = new Blocks (false);
+		Blocks upper = new Blocks (true);
 		
 		double bottomY = rg.nextDouble (0.6*APPLICATION_HEIGHT, 0.8*APPLICATION_HEIGHT);
 		double upperY = bottomY - rg.nextDouble(0.3*APPLICATION_HEIGHT, 0.6*APPLICATION_HEIGHT) - upper.getImg().getHeight();
