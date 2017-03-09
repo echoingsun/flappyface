a834667acf9ed2faeba5ca3b4b68e8875abe9848
S <s>
2017-03-08 16:14:08 -0800

    {"types":"C","runs":46}

--- PairComp.java
+++ PairComp.java
@@ -1,5 +1,9 @@
 import acm.graphics.GCompound;
 
 public class PairComp extends GCompound implements Constants{
+	
+	public PairComp (Pairs pairs){
+		this.add(pairs, blockPairs.getWidth() + BLOCK_INTERVAL, 0);
+	}
 
 }
