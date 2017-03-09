c275b82df264e52fa47c1814bbe5efc685fdd950
S <s>
2017-03-08 17:57:50 -0800

    {"types":"C","runs":79}

--- Contest_Run.java
+++ Contest_Run.java
@@ -23,7 +23,6 @@ public class Contest_Run extends Program implements Constants {
 	private Bird bird = new Bird();
 	private Pairs blockPairs1 = new Pairs();
 	private Pairs blockPairs2 = new Pairs();
-	private PairComp comp = new PairComp();
 
 	public void init() {
 		setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
@@ -33,17 +32,16 @@ public class Contest_Run extends Program implements Constants {
 		loadCanvas();
 		loadBird();
 		enableMouse();
-		blockPairs1 = sky.addBlockPairs(BLOCK_START_POINT);
-		blockPairs2 = sky.addBlockPairs(blockPairs1.getX() + blockPairs1.getWidth() + BLOCK_INTERVAL);
-
+		
+		loadBlocks();
+		
 		while (bird.notHit(sky)) {
 			bird.freeMove();
 			pause(DELAY);
 
 			if (blockPairs1.getX() + blockPairs1.getWidth() <= -BLOCK_INTERVAL) {
-				sky.remove(blockPairs1);
-				blockPairs1 = blockPairs2;
-				blockPairs2 = sky.addBlockPairs(blockPairs1.getX() + blockPairs1.getWidth() + +BLOCK_INTERVAL);
+				replaceBlockPairs();
+				
 			} else {
 				blockPairs1.moveLeft();
 
@@ -53,15 +51,19 @@ public class Contest_Run extends Program implements Constants {
 			blockPairs2.moveLeft();
 			pause(DELAY);
 
-			/*
-			 * if (sky.pairArray.get(0).getX() + BLOCK_WIDTH < 200){ blockPairs
-			 * = null; for (int i = 1; i < sky.pairArray.size(); i ++){
-			 * blockPairs.add(sky.pairArray.get(i)); }
-			 * sky.remove(sky.pairArray.get(0)); }
-			 */
-
 		}
 
+	}
+
+	private void replaceBlockPairs() {
+		sky.remove(blockPairs1);
+		blockPairs1 = blockPairs2;
+		blockPairs2 = sky.addBlockPairs(blockPairs1.getX() + blockPairs1.getWidth() + +BLOCK_INTERVAL);		
+	}
+
+	private void loadBlocks() {
+		blockPairs1 = sky.addBlockPairs(BLOCK_START_POINT);
+		blockPairs2 = sky.addBlockPairs(blockPairs1.getX() + blockPairs1.getWidth() + BLOCK_INTERVAL);
 	}
 
 	private void enableMouse() {
