72c939a0738fc02eb83bd88cab664c9c5f6baab7
S <s>
2017-03-08 18:08:25 -0800

    {"types":"C","runs":83}

--- Instructions.java
+++ Instructions.java
@@ -1,4 +1,17 @@
+import acm.graphics.GLabel;
 
-public class Instructions {
+public class Instructions extends GLabel implements Constants {
 
+	GLabel lbl;
+	public Instructions (String str, String content, int pts){
+		
+		if (str.equals("Instructions")){
+			lbl = new GLabel (content);
+			
+		}
+	}
+	
+	public GLabel getLbl (){
+		return lbl;
+	}
 }
