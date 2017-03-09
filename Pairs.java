577d5de96f82e94d91f20572e1b6d975e1e15c14
S <s>
2017-03-08 17:51:43 -0800

    {"types":"C","runs":79}

--- Pairs.java
+++ Pairs.java
@@ -1,13 +1,22 @@
+/*
+ * Class: Pairs.java
+ * "Pairs" is a GCompound that includes PAIR_NUM of blocks of random heights.
+ * It also has a method to move these blocks together.
+ */
+
 import acm.graphics.GCompound;
 import acm.util.RandomGenerator;
 
 public class Pairs extends GCompound implements Constants {
 
+	// Used to generate random heights for blocks.
 	RandomGenerator rg = new RandomGenerator();
-
 
 	public Pairs() {
 
+		// Generate PAIR_NUM of block pairs (upper and bottom).
+		// Each block will have a random height.
+		// The blocks are then added to this GCompound.
 		for (int i = 0; i < PAIR_NUM; i++) {
 			Blocks bottom = new Blocks(false);
 			Blocks upper = new Blocks(true);
@@ -16,14 +25,17 @@ public class Pairs extends GCompound implements Constants {
 			double upperY = bottomY - rg.nextDouble(0.3 * APPLICATION_HEIGHT, 0.5 * APPLICATION_HEIGHT)
 					- upper.getImg().getHeight();
 
-			this.add(upper.getImg(), BLOCK_INTERVAL + (BLOCK_INTERVAL + BLOCK_WIDTH)* i, upperY);
-			this.add(bottom.getImg(), BLOCK_INTERVAL + (BLOCK_INTERVAL + BLOCK_WIDTH)* i, bottomY);
+			this.add(upper.getImg(), BLOCK_INTERVAL + (BLOCK_INTERVAL + BLOCK_WIDTH) * i, upperY);
+			this.add(bottom.getImg(), BLOCK_INTERVAL + (BLOCK_INTERVAL + BLOCK_WIDTH) * i, bottomY);
 		}
 
 	}
 
+	/*
+	 * Method moveLeft enables the GCompound Pairs to move left.
+	 * This makes the blocks look like they move together in one.
+	 */
 	public void moveLeft() {
 		this.move(-MOVE_SPEED, 0);
-
 	}
 }
