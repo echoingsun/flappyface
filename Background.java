673f07d69c35a7ff4e803059e5ee918bb26fcece
S <s>
2017-03-09 00:42:02 -0800

    {"types":"C","runs":87}

--- Background.java
+++ Background.java
@@ -62,7 +62,7 @@ public class Background extends GCanvas implements Constants{
 		} else if (str.equals("Score")){
 			Displays youVeScored = new Displays("Score", "",0);
 			double x = this.getWidth() * 0.5 - youVeScored.getImg().getWidth() * 0.5;
-			double y = this.getHeight() * 0.5 - youVeScored.getImg().getHeight();
+			double y = this.getHeight() * 0.5 + youVeScored.getImg().getHeight();
 			this.add (youVeScored.getImg(), x, y);
 			return youVeScored;
 		}
