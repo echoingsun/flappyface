7f2ebcb0c87608b2562b9c69776228ac60369f76
S <s>
2017-03-09 00:50:52 -0800

    {"types":"C","runs":95}

--- Displays.java
+++ Displays.java
@@ -6,14 +6,14 @@ public class Displays implements Constants {
 	private GLabel lbl;
 	private GImage img;
 	
-	public Displays (String str, String content, int pts){
+	public Displays (String str, int pts){
 		
 		switch (str){
 		case "Title" : img = new GImage ("FlappyFaceTitle.png"); break;
 		case "ClickToStart" : img = new GImage ("clicktostart.png"); break;
 		case "GameOver" : img = new GImage ("gameover.png"); break;
 		case "Score" : img = new GImage("score.png"); break;
-		case "Points" : lbl = new GLabel( Integer.toString(pts));
+		case "Points" : lbl = new GLabel(Integer.toString(pts));
 
 		}
 		
