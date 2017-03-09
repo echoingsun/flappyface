6e7cbc1d9f921390083438d9c21c9595624f2a65
S <s>
2017-03-08 16:15:09 -0800

    {"types":"C","runs":46}

--- PairComp.java
+++ PairComp.java
@@ -2,8 +2,15 @@ import acm.graphics.GCompound;
 
 public class PairComp extends GCompound implements Constants{
 	
+	RandomGenerator rg = new RandomGenerator();
+	double vx = rg.nextDouble(-MOVE_MAX_SPEED, -MOVE_MIN_SPEED);
+	
 	public PairComp (Pairs pairs){
 		this.add(pairs, pairs.getWidth() + BLOCK_INTERVAL, 0);
 	}
 
+	public void moveLeft() {
+		this.move(vx, 0);
+
+	}
 }
