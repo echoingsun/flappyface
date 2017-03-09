3d4d28cd85f8cdb3005f35502e4b0ff12f98c1c7
S <s>
2017-03-09 00:22:00 -0800

    {"types":"C","runs":81}

--- Constants.java
+++ Constants.java
@@ -30,6 +30,9 @@ public interface Constants {
 	public static final int TITLE_UPPER_BORDER = 200;
 	public static final int TITLE_LOWER_BORDER = 206;
 	
+	public static final int GAMEOVER_UPPER_BORDER = 306;
+	public static final int GAMEOVER_LOWER_BORDER = 316;
+	
 	public static final int PAIR_NUM = 3;
 	
 	public static final int PTS_DELTA = 100;
