1947c44a796f93f072292ef015bcbff12a1bfb73
S <s>
2017-03-09 18:41:50 -0800

    {"types":"C","runs":98}

--- Blocks.java
+++ Blocks.java
@@ -1,27 +1,35 @@
+
+/*
+ * File: Blocks.java
+ * ----------------------------
+ * This class defines a single block.
+ * These blocks will be used by a GCompound class, "Pairs".
+ */
+
 import acm.graphics.*;
 import acm.util.RandomGenerator;
 
 public class Blocks implements Constants {
 
+	// Each block has an image to it.
 	private GImage img;
 
+	// Constructor: generate a block.
+	// There're bottom blocks and upper blocks, and each has a different image.
+	// By passing a boolean value, there can be upper and bottom blocks.
 	public Blocks(boolean isFlip) {
-
 		GImage upperBlock = new GImage("pipeFlip.png");
 		GImage bottomBlock = new GImage("pipe.png");
 
 		if (isFlip == true) {
 			img = upperBlock;
-
 		} else {
 			img = bottomBlock;
 		}
-
 	}
 
+	// Image getter.
 	public GImage getImg() {
 		return img;
 	}
-
-
 }
